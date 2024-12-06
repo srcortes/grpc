4694c2b6d424ec72ffa10e765730b762eb911c43
john.rodriguez <john.rodriguez@gmail.com>
2024-12-05 22:26:38 -0500

    Create conmunication betmween client server with grpc

--- grpc-playground/src/main/java/sec06/GrpcClient.java
+++ grpc-playground/src/main/java/sec06/GrpcClient.java
@@ -14,7 +14,7 @@ public class GrpcClient {
     public static void main(String[] args) {
         var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
                 .usePlaintext().build();
-        var stub = BankServiceGrpc.newBlockingStub(channel);
+        BankServiceGrpc.BankServiceBlockingStub stub = BankServiceGrpc.newBlockingStub(channel);
         var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder()
                         .setAccountNumber(10)
                 .build());
