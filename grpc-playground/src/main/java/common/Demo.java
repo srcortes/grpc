49c1067253852f62e836adbe1e5150c02f993069
john.rodriguez <john.rodriguez@gmail.com>
2025-02-21 20:43:15 -0500

    practicing grpc

--- grpc-playground/src/main/java/common/Demo.java
+++ grpc-playground/src/main/java/common/Demo.java
@@ -1,14 +1,15 @@
 package common;
 
-import sec06.BankService;
+
 import sec06.TransferService;
 import sec07.FlowControlService;
+import sec09.BankService;
 
 import java.io.IOException;
 
 public class Demo {
     public static void main(String[] args) throws IOException, InterruptedException {
-        GrpcServer.create( new FlowControlService())
+        GrpcServer.create( new BankService())
                 .start().await();
     }
 }
--- grpc-playground/src/main/java/sec09/BankService.java
+++ grpc-playground/src/main/java/sec09/BankService.java
@@ -5,42 +5,42 @@ import com.grpc.models.sec09.AccountBalance;
 import com.grpc.models.sec09.BalanceCheckRequest;
 import com.grpc.models.sec09.BankServiceGrpc;
 import com.grpc.models.sec09.WithDrawRequest;
+import com.grpc.models.sec09.Money;
 
 import io.grpc.stub.StreamObserver;
 import io.grpc.Status;
-import sec06.repository.AccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sec09.repository.AccountRepository;
 import sec09.validator.RequestValidator;
+
+
 
 import java.util.concurrent.TimeUnit;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
+    private static final Logger log = LoggerFactory.getLogger(BankService.class);
+
     @Override
     public void getAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
         RequestValidator.validateAccount(request.getAccountNumber())
                 .map(Status::asRuntimeException)
-                .ifPresentOrElse(status -> responseObserver.onError(status.asRuntimeException()),
+                .ifPresentOrElse(responseObserver::onError,
                         () -> sendAccountBalance(request, responseObserver));
     }
 
     @Override
     public void withDraw(WithDrawRequest request, StreamObserver<com.grpc.models.sec09.Money> responseObserver) {
-        var accountNumber = request.getAccountNumber();
-        var requestAmount = request.getAmount();
-        var accountBalance = AccountRepository.getBalance(accountNumber);
+        RequestValidator.validateAccount(request.getAccountNumber())
+                .or(()-> RequestValidator.isAmountDivisibleBy10(request.getAmount()))
+                .or(()-> RequestValidator
+                        .hasSufficientBalance(request.getAmount(),
+                                AccountRepository.getBalance(request.getAccountNumber())))
+                .map(Status::asRuntimeException)
+                .ifPresentOrElse(responseObserver::onError,
+                        ()-> sendMoney(request, responseObserver));
 
-        if(requestAmount > accountBalance){
-            responseObserver.onCompleted();
-        }
-
-        for(int j = 0; j < (requestAmount / 10); j++){
-            var money = Money.newBuilder()
-                    .setAmount(10).build();
-            responseObserver.onNext(money);
-            AccountRepository.deductAmount(accountNumber, 10);
-            Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
-        }
-        responseObserver.onCompleted();
     }
 
     private void sendAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
@@ -52,4 +52,35 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
         responseObserver.onNext(accountBalance);
         responseObserver.onCompleted();
     }
+
+    private void sendMoney(WithDrawRequest request, StreamObserver<com.grpc.models.sec09.Money> responseObserver) {
+        try{
+            var accountNumber = request.getAccountNumber();
+            var requestAmount = request.getAmount();
+            var accountBalance = AccountRepository.getBalance(accountNumber);
+
+            if(requestAmount > accountBalance){
+                responseObserver.onCompleted();
+                return;
+            }
+
+            for(int j = 0; j < (requestAmount / 10); j++){
+                var money = Money.newBuilder()
+                        .setAmount(10).build();
+                if(j == 3)
+                    throw new RuntimeException();
+
+                responseObserver.onNext(money);
+                AccountRepository.deductAmount(accountNumber, 10);
+                Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+            }
+            responseObserver.onCompleted();
+
+        }catch(Exception ex){
+            responseObserver.onError(Status.INTERNAL.withDescription(ex.getMessage()).asRuntimeException());
+        }
+
+    }
+
+
 }
