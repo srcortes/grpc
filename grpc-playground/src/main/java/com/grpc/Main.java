a94bf8388a56c49464a761a5334ea511aa09f249
srcortes <john.rodriguez.cortes@gmail.com>
2025-11-28 20:51:36 -0500

    practice class 8

---
+++ grpc-playground/src/main/java/com/grpc/Main.java
@@ -0,0 +1,13 @@
+package com.grpc;
+import com.grpc.models.sec01.Persons;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Persons.newBuilder()
+                .setName("John Doe")
+                .setAge(30)
+                .setEmail("
+
+    }
+}
---
+++ grpc-playground/src/main/java/com/grpc/sec01/SimpleProtoDemo.java
@@ -0,0 +1,13 @@
+package com.grpc.sec01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.grpc.models.sec01.Persons;
+
+public class SimpleProtoDemo {
+    private static final Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
+
+    public static void main(String[] args) {
+        new Persons.
+    }
+}
